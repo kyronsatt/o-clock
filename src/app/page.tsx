6b3da2ed0d9ca40fd478b7932940f2a0bf0db210
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-06 13:25:05 -0300

    feat: functional clock behaviour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,119 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
+interface IClockPointerLocationClassNames {
+  bottom: string;
+  left: string;
+  rotate: string;
+}
+
+export default function ClockPage() {
+  const [currentTime, setCurrentTime] = useState<Date>();
+  const [clockPointerLocationClassNames, setClockPointerLocationClassNames] =
+    useState<IClockPointerLocationClassNames>({
+      bottom: "",
+      left: "",
+      rotate: "",
+    });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // const test = currentTime;
+      // test?.setHours(currentTime.getHours() + 1);
+
+      const currentTime = new Date();
+      setCurrentTime(currentTime);
+
+      const { bottom, left, rotate } =
+        getClockPointerPositionClassNames(currentTime);
+
+      setClockPointerLocationClassNames({
+        bottom,
+        left,
+        rotate,
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []); // Run effect only once on component mount
+
+  const getClockPointerPositionClassNames = (
+    time: Date
+  ): IClockPointerLocationClassNames => {
+    // Extract hours and minutes from the current date and time
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+
+    // Calculate total minutes elapsed since 0h
+    const totalMinutes = hours * 60 + minutes;
+
+    // Calculate the angle based on the total minutes elapsed
+    const angle = (totalMinutes / 1440) * 360; // Total minutes in a day = 1440
+
+    // Calculate x and y coordinates using trigonometry
+    const radius = 50; // Assuming the clock radius is 50 units
+    const centerX = 50; // Assuming the center of the clock is at (50, 50)
+    const centerY = 50;
+
+    const radians = angle * (Math.PI / 180);
+
+    const x = centerX + radius * Math.sin(radians);
+    const y = centerY + radius * Math.cos(radians);
+
+    // Convert x and y coordinates to percentages
+    let xPercentage = (1 - x / (2 * radius)) * 100;
+    let yPercentage = (1 - y / (2 * radius)) * 100;
+    if (angle <= 130) {
+      xPercentage -= 1.8;
+      yPercentage -= 1.8;
+    } else {
+      xPercentage -= 2;
+      yPercentage -= 2;
+    }
+
+    const rotationDegrees = angle - 90;
+
+    const bottomPositionInPercentualString = `${yPercentage}%`;
+    const leftPositionInPercentualString = `${xPercentage}%`;
+    const rotationInDegreesString = `${rotationDegrees}deg`;
+
+    return {
+      bottom: bottomPositionInPercentualString,
+      left: leftPositionInPercentualString,
+      rotate: rotationInDegreesString,
+    };
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex h-screen w-screen flex-col items-center justify-between px-6 py-24">
+      <div className="h-[90%] relative flex aspect-square">
+        <div className="h-full absolute aspect-square rounded-full border-2 border-solid border-dark"></div>
+        <div className="flex flex-col justify-center align-middle self-center text-center w-full">
+          <div className="text-7xl font-extralight">O'Clock</div>
+          <div className="font-light mt-4">
+            <div className="text-2xl">{currentTime?.toLocaleTimeString()}</div>
+            <div className="text-xs">{currentTime?.toLocaleDateString()}</div>
+          </div>
         </div>
-      </div>
+        <div className="absolute w-full h-full text-[10px] font-light">
+          <div className="absolute bottom-1/2 left-4">6h</div>
+          <div className="absolute top-4 left-1/2">12h</div>
+          <div className="absolute bottom-1/2 right-4">18h</div>
+          <div className="absolute bottom-4 left-1/2">24h</div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {currentTime && (
+          <Image
+            src={"pointer.svg"}
+            height={16}
+            width={16}
+            alt="clock-pointer"
+            className="absolute origin-center"
+            style={{ ...clockPointerLocationClassNames }}
+          />
+        )}
       </div>
     </main>
   );
