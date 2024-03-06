c886e8cde0fb5f655dcc46e162970d398c5ef6bf
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-06 13:24:47 -0300

    chore: configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "dark": "#2E2E2E"
+      }
     },
   },
   plugins: [],
