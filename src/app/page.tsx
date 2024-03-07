76f5db97a533a809e89c999732a79379085d151e
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-07 12:34:03 -0300

    refactor: remove unused features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,18 @@ import DynamicBackground from "./ui/dynamic-background";
 import Clock from "./ui/clock";
 
 export default function ClockPage() {
-  const [currentTime, setCurrentTime] = useState<Date>(new Date());
-  const [primaryColor, setPrimaryColor] = useState<string>();
+  const [currentTime, setCurrentTime] = useState<Date>();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      // const test = currentTime;
-      // test?.setHours(currentTime.getHours() + 1);
-
-      // // const currentTime = new Date();
-      // setCurrentTime(test);
-
       const newTime = new Date();
       setCurrentTime(newTime);
-
-      const primaryColor = getPrimaryColor(newTime);
-      setPrimaryColor(primaryColor);
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  const getPrimaryColor = (currentTime: Date) => {
-    const hours = currentTime?.getHours();
-    if (hours >= 18 || hours < 5) {
-      return "#FFFFFF";
-    } else {
-      return "#2E2E2E";
-    }
-  };
+  if (!currentTime) return <></>;
 
   return (
     <DynamicBackground currentTime={currentTime}>
