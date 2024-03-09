5618d9da3f7d091b326916d1563231989c6acf38
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-09 17:52:10 -0300

    feat: static menu

--- src/app/ui/clock/content.tsx
+++ src/app/ui/clock/content.tsx
@@ -1,24 +1,27 @@
 "use client";
 
+import Menu from "../menu";
+
 interface IClockContent {
   time: Date;
 }
 
 export default function ClockContent({ time }: IClockContent) {
   return (
-    <div className="absolute z-10 mt-8">
+    <div className="absolute z-10 pt-24">
       <div className="flex flex-col justify-center align-middle self-center text-center w-full text-white">
         <div className="text-6xl font-thin tracking-widest text-shadow-xl shadow-[#FFFFFF60]">
           O'Clock
         </div>
         <div className="flex flex-col justify-center items-center font-light mt-2 px-3 tracking-wider opacity-80">
-          <div className="text-xl w-full text-center">
+          <div className="text-xl w-full font-medium text-center">
             {time?.toLocaleTimeString()}
           </div>
           <div className="text-xs w-full text-center font-extralight">
             {time?.toLocaleDateString()}
           </div>
         </div>
+        <Menu />
       </div>
     </div>
   );
