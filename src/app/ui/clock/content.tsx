0ab62cff1fa7e1433a8317db169cce35f2cbed26
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-09 10:46:37 -0300

    feat: improve clock context deisng

--- src/app/ui/clock/content.tsx
+++ src/app/ui/clock/content.tsx
@@ -6,14 +6,16 @@ interface IClockContent {
 
 export default function ClockContent({ time }: IClockContent) {
   return (
-    <div className="absolute z-10 text-dark">
+    <div className="absolute z-10 text-white">
       <div className="flex flex-col justify-center align-middle self-center text-center w-full">
-        <div className="text-7xl font-light">O'Clock</div>
-        <div className="font-light mt-4">
+        <div className="text-7xl font-light tracking-widest text-shadow-lg shadow-[#FFFFFF80]">
+          O'Clock
+        </div>
+        <div className="flex flex-col justify-center items-center font-regular mt-4 px-3 tracking-wider">
           <div className="text-2xl w-full text-center">
             {time?.toLocaleTimeString()}
           </div>
-          <div className="text-xs w-full text-center">
+          <div className="text-sm w-full text-center font-light">
             {time?.toLocaleDateString()}
           </div>
         </div>
