e34d06a9cd4543faebf878de107a26083b938620
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2024-03-12 09:20:02 -0300

    feat: login page

---
+++ src/app/entrar/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center p-24 bg-white">
+      <div className="flex flex-col items-center justify-center h-full aspect-square rounded-full border-4 border-dark bg-transparent">
+        <div className="text-6xl font-thin tracking-widest text-shadow-xl shadow-[#FFFFFF60]">
+          O&apos;Clock
+        </div>
+        <button
+          className="flex items-center justify-between gap-2 mt-6 px-4 py-2 rounded-3xl bg-gradient-to-r from-[#FF3D3D] to-[#FF862E] font-light text-white"
+          onClick={() => signIn("google")}
+        >
+          <Image
+            src={"/google-logo.png"}
+            alt="google-logo"
+            width={20}
+            height={20}
+          />
+          Entrar com o Google
+        </button>
+      </div>
+    </main>
+  );
+}
--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Login Page
-    </main>
-  );
-}
